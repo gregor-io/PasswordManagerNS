5c02a4581798c5c994889a8e207d8e18ccaad784
tyler.gregory <me@tcgregory.dev>
2024-07-03 11:58:22 -0600

    First commit.

---
+++ PasswordEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace PasswordManagerNS;
+
+public class PasswordEntry
+{
+    public string? Url { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
